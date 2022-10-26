4f22503578d2f1e506d9ed0d8d64c30105ecb913
So Yeon Kim <47154546+72Soyeon@users.noreply.github.com>
2022-10-25 21:40:49 -0400

    1025 JSW update
    Updated

--- 1025 JSW/Ch3.js
+++ 1025 JSW/Ch3.js
@@ -6,30 +6,43 @@
 
 let month = prompt("Enter month in number: ");
 
-if(number=1){
-    console.log('January has 31 days');
-} else if(number=2){
-    console.log('February has 28 days');
-}else if(number=3){
-    console.log('March has 31 days');
-}else if(number=4){
-    console.log('April has 30 days');
-}else if(number=5){
-    console.log('May has 31 days');
-}else if(number=6){
-    console.log('June has 30 days');
-}else if(number=7){
-    console.log('July has 31 days');
-}else if(number=8){
-    console.log('August has 31 days');
-}else if(number=9){
-    console.log('September has 30 days');
-}else if(number=10){
-    console.log('October has 31 days');
-}else if(number=11){
-    console.log('November has 30 days');
-}else if(number=12){
-    console.log('December has 31 days');
-}else{
-    console.log('${month} is a wrong number');
-}
+switch (month) {
+    case "1":
+        console.log("January has 31 days");
+        break
+    case "2":
+        console.log('February has 28 days');
+        break;
+    case "3":
+        console.log("March has 31 days");
+        break
+    case "4":
+        console.log('April has 30 days');
+        break;
+    case "5":
+        console.log("May has 31 days");
+        break
+    case "6":
+        console.log('June has 30 days');
+        break;
+    case "7":
+        console.log("July has 31 days");
+        break
+    case "8":
+        console.log('August has 31 days');
+        break;
+    case "9":
+        console.log("September has 30 days");
+        break
+    case "10":
+        console.log('October has 31 days');
+        break;
+    case "11":
+        console.log("November has 30 days");
+        break
+    case "12":
+        console.log('December has 31 days');
+        break;
+    default:
+        console.log('${month} is not a valid input');
+  }
